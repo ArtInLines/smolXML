15e5af0cd4fa7f3438ae3bd74ce224741462b26a
ArtInLines <linesinart@gmail.com>
2024-08-17 23:37:03 +0200

    move test.py to root dir

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import smolXML as xml
 
-root = xml.parseFile("big_test.xml")
+root = xml.parseFile("test/big_test.xml")
 
 preamble = root["statement"]["preamble"]
 containers = preamble.getAllChildrenByName("container")
